8719b0b2cb27a9e5e97ac9b3c6396ef5d5b16a2a
デンパ時計 <jsoldenpa@yahoo.co.jp>
2021-09-21 20:57:57 +0900

    プロジェクト ファイルを追加します。

---
+++ BSPlayerDataBackup/Configuration/PluginConfig.cs
@@ -0,0 +1,41 @@
+﻿using IPA.Config.Stores;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo(GeneratedStore.AssemblyVisibilityTarget)]
+namespace BSPlayerDataBackup.Configuration
+{
+    public class PluginConfig
+    {
+        public static PluginConfig Instance { get; set; }
+
+        public virtual string BackupPath { get; set; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "BSPlayerDataBackup");
+
+        /// <summary>
+        /// This is called whenever BSIPA reads the config from disk (including when file changes are detected).
+        /// </summary>
+        public virtual void OnReload()
+        {
+
+        }
+        /// <summary>
+        /// Call this to force BSIPA to update the config file. This is also called by BSIPA if it detects the file was modified.
+        /// </summary>
+        public virtual void Changed()
+        {
+            // Do stuff when the config is changed.
+        }
+        /// <summary>
+        /// Call this to have BSIPA copy the values from <paramref name="other"/> into this config.
+        /// </summary>
+        public virtual void CopyFrom(PluginConfig other)
+        {
+            
+        }
+    }
+}
---
+++ BSPlayerDataBackup/Plugin.cs
@@ -0,0 +1,83 @@
+﻿using BSPlayerDataBackup.Configuration;
+using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using IPALogger = IPA.Logging.Logger;
+
+namespace BSPlayerDataBackup
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+
+        [Init]
+        /// <summary>
+        /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
+        /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
+        /// Only use [Init] with one Constructor.
+        /// </summary>
+        public void Init(IPALogger logger, Config config)
+        {
+            Instance = this;
+            Log = logger;
+            Log.Info("BSPlayerDataBackup initialized.");
+            PluginConfig.Instance = config.Generated<PluginConfig>();
+        }
+        [OnEnable]
+        public void OnEnable()
+        {
+            var descPath = Path.Combine(PluginConfig.Instance.BackupPath, $"{Application.version}_{DateTime.Now:yyyyMMdd}");
+            var tmpPath = Path.Combine(Path.GetTempPath(), "BSPlayerDataBackup");
+            try {
+                if (!Directory.Exists(Path.GetDirectoryName(descPath))) {
+                    Directory.CreateDirectory(Path.GetDirectoryName(descPath));
+                }
+                Log.Info($"Start backup : {descPath}");
+                this.Copy(Application.persistentDataPath, tmpPath);
+                if (File.Exists($"{descPath}.zip")) {
+                    File.Delete($"{descPath}.zip");
+                }
+                ZipFile.CreateFromDirectory(tmpPath, $"{descPath}.zip");
+            }
+            catch (Exception e) {
+                Log.Error(e);
+            }
+            finally {
+                if (Directory.Exists(tmpPath)) {
+                    try {
+                        Directory.Delete(tmpPath, true);
+                    }
+                    catch (Exception e) {
+                        Log.Error(e);
+                    }
+                }
+            }
+        }
+
+        private void Copy(string sourceDirectoryName, string destDirectoryName, bool overwrite = false)
+        {
+            if (!Directory.Exists(destDirectoryName)) {
+                Directory.CreateDirectory(destDirectoryName);
+            }
+            foreach (var file in Directory.EnumerateFiles(sourceDirectoryName, "*", SearchOption.TopDirectoryOnly)) {
+                if (string.Equals(new FileInfo(file).Directory.Name, "BSPlayerDataBackup", StringComparison.InvariantCultureIgnoreCase)) {
+                    return;
+                }
+                File.Copy(file, Path.Combine(destDirectoryName, Path.GetFileName(file)), overwrite);
+            }
+            foreach (var dir in Directory.EnumerateDirectories(sourceDirectoryName, "*", SearchOption.TopDirectoryOnly)) {
+                this.Copy(dir, Path.Combine(destDirectoryName, Path.GetFileName(dir)), overwrite);
+            }
+        }
+    }
+}
